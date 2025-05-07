fd1be34ac4e8383d9c91a5f85fa528525ed18c73
Ankit <ankitparmarranila@gmail.com>
2025-05-07 18:20:25 +0530

    Fix cors error

--- src/server.js
+++ src/server.js
@@ -3,11 +3,25 @@ import dotenv from 'dotenv';
 import { connectDB } from './config/database.js';
 import entityManager from './models/entityManager.js';
 import APIrouter from './API-gateway.js';
-
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
 dotenv.config();
 
 const app = express();
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}))
 app.use(express.json());
+app.use(cookieParser());
+app.use(session({
+    secret: process.env.SESSION_SECRET || 'your-secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+}));
+
 // Routes
 app.use("/", APIrouter);
 const PORT = process.env.PORT || 5005;
